d71334c33ef1a862d3916f21c1d76b5312866bf5
green4984 <green.4984@gmail.com>
2016-03-15 23:11:41 +0800

    fix bugs, add create_api, init_app like flask extension

--- dynamic_obj/dy_manager.py
+++ dynamic_obj/dy_manager.py
@@ -7,7 +7,7 @@ from .dynamic_object_base import DynamicObjectBase
 
 class DyManager(object):
     __blueprints = {}
-    __form_class = {}
+    app = None
     def __init__(self, app=None):
         if app:
             self.app = app
@@ -20,28 +20,31 @@ class DyManager(object):
         return getattr(cls, '_instance')
 
     def init_app(self, app=None):
-        if app:
-            self.app = app
-        for name, bp_dict in self.__blueprints.iteritems():
-            for bp, val in bp_dict.iteritems():
-                if not val:
-                    val = True
-                    self.app.register_blueprint(bp)
+        assert app
+        self.app = app
+        for name, bp_list in self.__blueprints.iteritems():
+            self.__register_blueprint(name, bp_list[0], app)
 
     def create_api(self, object_class, **kwargs):
-        app = kwargs.pop('app', None)
-        self.create_api_blueprint(object_class, **kwargs)
-        self.init_app(app)
+        return self.create_api_blueprint(object_class, **kwargs)
 
-    def create_api_blueprint(self, obj_class, url_prefix='/api'):
+    def create_api_blueprint(self, obj_class, url_prefix='/api', app=None):
         """ register blueprint """
         assert issubclass(obj_class, DynamicObjectBase)
         name = getattr(obj_class, "__obj_name__", obj_class.__name__).lower()
         if name in self.__blueprints:
-            return self.__blueprints.get(name).keys()[0]
+            return self.__blueprints.get(name)[0]
         blueprint = Blueprint(obj_class.__name__, __name__, url_prefix=url_prefix)
         view_func = obj_class.as_view(name)
         blueprint.add_url_rule('/{0}'.format(name), view_func=view_func)
         blueprint.add_url_rule('/{0}/'.format(name), view_func=view_func)
-        self.__blueprints.setdefault(name, {blueprint: False})
+        self.__register_blueprint(name, blueprint, app)
         return blueprint
+
+    def __register_blueprint(self, name, blueprint, app=None):
+        app = app or self.app
+        if name not in self.__blueprints:
+            self.__blueprints.setdefault(name, [blueprint, False])
+        if app and not self.__blueprints[name][1]:
+            self.__blueprints[name][1] = True
+            app.register_blueprint(blueprint)
